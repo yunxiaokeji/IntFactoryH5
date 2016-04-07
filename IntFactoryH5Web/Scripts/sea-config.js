31fdc2011665157abcdeda21b940ca56a50d0ac9
sunallen521 <sunallen@163.com>
2016-04-07 10:14:20 +0800

    初始化

---
+++ IntFactoryH5Web/Scripts/sea-config.js
@@ -0,0 +1,27 @@
+﻿
+/*基础配置*/
+seajs.config({
+    base: "/modules/",
+    paths: {
+       
+    },
+    alias: {
+        "jquery": "/Scripts/jquery-1.11.1.js",
+        "global": "scripts/global.js",
+        //HTML模板引擎
+        "dot": "plug/doT.js",
+    },
+    map: [
+        //可配置版本号
+        ['.css', '.css?v=20160406'],
+        ['.js', '.js?v=20160406']
+    ]
+});
+
+
+seajs.config({
+    alias: {
+       
+    }
+});
+
