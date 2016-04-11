f7f2d710e234930ae2bb6a8ee84e82bb064e513a
song19940906 <2079973693@qq.com>
2016-04-11 15:51:34 +0800

    4.11日

--- IntFactoryH5Web/modules/scripts/task/tasklist.js
+++ IntFactoryH5Web/modules/scripts/task/tasklist.js
@@ -1,69 +1,47 @@
 ﻿define(function (require,exports,module) {
-    var myListTask = {};
-    myListTask.init = function () {
-        myListTask.bindEvent();
+    var MyListTask = {};
+    MyListTask.init = function () {
+        MyListTask.bindEvent();
     };
-    myListTask.bindEvent = function () {
-        //鼠标滑动事件
-        $(".all").hover(function () {
-            $(".all-a").css("color", "#007aff").css("cursor", "pointer");
-            $(".iconfont-all").css("color", "#007aff").css("cursor", "pointer");
+    MyListTask.bindEvent = function () {
+        //遮罩
+        $("#cancel-header").click(function () {
+            $(".shade").css("display", "block");
         });
-        $(".unreceived").hover(function () {
-            $(".unreceived-a").css("color", "#007aff").css("cursor", "pointer");
-            $(".inquadrangle").css("background-color", "#007aff").css("cursor", "pointer");
-            $(".exround").css("border", "1px solid #007aff").css("cursor", "pointer");
+        $(".cencal").click(function () {
+            $(".shade").css("display", "none");
         });
-        $(".underway").hover(function () {
-            $(".underway-a").css("color", "#007aff").css("cursor", "pointer");
-            $("#iconfont-underway").css("color", "#007aff").css("cursor", "pointer");
+        //类型
+        $("#type-a").click(function () {
+            var num = $("#type-a").data("type");
+            if (num == "0") {
+                $(".option").css("display", "block");
+                $("#type-a").data("type", "1");
+            } else {
+                $(".option").css("display", "none");
+                $("#type-a").data("type", "0");
+            }
         });
-        $(".accomplish").hover(function () {
-            $(".accomplish-a").css("color", "#007aff").css("cursor", "pointer");
-            $("#iconfont-underway-accom").css("color", "#007aff").css("cursor", "pointer");
+        //高级筛选       
+        $("#select-copy").click(function () {
+            //$(".tab-screen").slidetoggle();
+            var num = $("#select-copy").data("select");
+            if (num=="0") {
+                $("#tab-screen").css("display", "block");
+                $("#select-copy").data("select","1");
+            } else {
+                $("#tab-screen").css("display", "none");
+                $("#select-copy").data("select", "0");
+            }            
         });
-        $(".type").hover(function () {
-            $(".type-a").css("color", "#007aff").css("cursor", "pointer");
-            $("#iconfont-type").css("color", "#007aff").css("cursor", "pointer");
-        });
-        $(".flow").hover(function () {
-            $(".flow-a").css("color", "#007aff").css("cursor", "pointer");
-            $("#iconfont-flow").css("color", "#007aff").css("cursor", "pointer");
-        });
-        $(".screen").hover(function () {
-            $(".screen-a").css("color", "#007aff").css("cursor", "pointer");
-            $("#iconfont-screen").css("color", "#007aff").css("cursor", "pointer");
-        });
-        //鼠标离开事件
-        $(".all").mouseleave(function () {
-            $(".all-a").css("color", "#666");
-            $(".iconfont-all").css("color", "#666");
-        });
-        $(".unreceived").mouseleave(function () {
-            $(".unreceived-a").css("color", "#666");
-            $(".inquadrangle").css("background-color", "#666");
-            $(".exround").css("border", "1px solid #666");
-        });
-        $(".underway").mouseleave(function () {
-            $(".underway-a").css("color", "#666");
-            $("#iconfont-underway").css("color", "#666");
-        });
-        $(".accomplish").mouseleave(function () {
-            $(".accomplish-a").css("color", "#666");
-            $("#iconfont-underway-accom").css("color", "#666");
-        });
-        $(".type").mouseleave(function () {
-            $(".type-a").css("color", "#999");
-            $("#iconfont-type").css("color", "#999");
-        });
-        $(".flow").mouseleave(function () {
-            $(".flow-a").css("color", "#999");
-            $("#iconfont-flow").css("color", "#999");
-        });
-        $(".screen").mouseleave(function () {
-            $(".screen-a").css("color", "#999");
-            $("#iconfont-screen").css("color", "#999");
-        });
+
+
+        
+        
+
+
+
+
     };
-    module.exports = myListTask;
+    module.exports = MyListTask;
 });
