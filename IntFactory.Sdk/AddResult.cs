c4c211dc1c2f1bdc430d523c0e86a1e8148861d8
xiaoxiaomuge <xiaoxiaomuge@163.com>
2016-04-11 16:35:55 +0800

    调整任务接口

---
+++ IntFactory.Sdk/AddResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntFactory.Sdk
+{
+    public class AddResult
+    {
+        public string id;
+
+        public int error_code = 0;
+
+        public string error_message;
+    }
+}
