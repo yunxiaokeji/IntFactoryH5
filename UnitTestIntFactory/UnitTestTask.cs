139cc05072f0f303d0a5cded7ac274f8d72f795e
xiaoxiaomuge <xiaoxiaomuge@163.com>
2016-04-11 15:25:51 +0800

    新增智能工厂sdk和单元测试项目

---
+++ UnitTestIntFactory/UnitTestTask.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using IntFactory.Sdk;
+namespace UnitTestIntFactory
+{
+    [TestClass]
+    public class UnitTestTask
+    {
+        string userID = "BC6802E9-285C-471C-8172-3867C87803E2";
+        string agentID = "9F8AF979-8A3B-4E23-B19C-AB8702988466";
+
+        [TestMethod]
+        public void GetTasks()
+        {
+            var result= TaskBusiness.BaseBusiness.GetTasks(userID, agentID);
+            Assert.IsTrue(result.error_code==0);
+        }
+    }
+}
