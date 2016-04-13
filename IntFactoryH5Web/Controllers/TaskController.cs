2fc2348aa7e695333f39d8ba079a000f039206a0
w609365252 <609365252@qq.com>
2016-04-13 10:55:25 +0800

    任务详情数据添加

--- IntFactoryH5Web/Controllers/TaskController.cs
+++ IntFactoryH5Web/Controllers/TaskController.cs
@@ -13,16 +13,18 @@ namespace IntFactoryH5Web.Controllers
 
         string userID = "BC6802E9-285C-471C-8172-3867C87803E2";
         string agentID = "9F8AF979-8A3B-4E23-B19C-AB8702988466";
-        Dictionary<string, object> TaskDetail = new Dictionary<string, object>();
+        string TaskID = "8e266ab4-0ff6-499b-89ce-d2e3454be123";
+        Dictionary<string, object> JsonResult = new Dictionary<string, object>();
         public ActionResult List()
         {
             return View();
         }
+
         //浏览器加载获取任务详情信息
         public ActionResult Detail(string taskID)
         {
 
-            taskID = "8e266ab4-0ff6-499b-89ce-d2e3454be123";
+            taskID = TaskID;
             Dictionary<string, object> resultTaskInfoObj = new Dictionary<string, object>();
 
             var resultTask = IntFactory.Sdk.TaskBusiness.BaseBusiness.GetTaskDetail(taskID, userID, agentID);
@@ -38,11 +40,6 @@ namespace IntFactoryH5Web.Controllers
                 {
                     var task = resultTask.task;
                     ViewBag.Task = task;
-                    
-                    //var resultTaskReply = IntFactory.Sdk.TaskBusiness.BaseBusiness.GetTaskReplys(resultTask.task.orderID, resultTask.task.stageID, userID, agentID, 1);
-
-                    //ViewBag.TaskReplys = resultTaskReply.taskReplys;
-                   
                 }
             }
             return View();
@@ -51,19 +48,55 @@ namespace IntFactoryH5Web.Controllers
         //获取讨论列表   
         public JsonResult GetDiscussInfo(string orderID, string stageID,int pageIndex)
         {
-
             var result = IntFactory.Sdk.TaskBusiness.BaseBusiness.GetTaskReplys(orderID, stageID, userID, agentID, pageIndex);
 
             List<IntFactory.Sdk.TaskReplyEntity> listReplys = result.taskReplys;
 
-            return new JsonResult { 
-            
-                Data=listReplys,
+            JsonResult.Add("items", listReplys);
+
+            return new JsonResult {
+
+                Data = JsonResult,
 
                 JsonRequestBehavior=JsonRequestBehavior.AllowGet
 
             };
         }
+
+        //获取日志列表
+        public JsonResult GetLogInfo(string taskID)
+        {
+            taskID = TaskID;
+
+            var result = IntFactory.Sdk.TaskBusiness.BaseBusiness.GetTaskLogs(taskID, userID, agentID, 1);
+
+            List<IntFactory.Sdk.TaskLogEntity> logLists=result.taskLogs;
+
+            JsonResult.Add("items",logLists);
+
+            return new JsonResult {
+
+                Data = JsonResult,
+                JsonRequestBehavior=JsonRequestBehavior.AllowGet
+            
+            };
+
+        }
+
+        //获取材料采购计划列表
+        public JsonResult GetOrderInfo(string orderID)
+        {
+            var result = IntFactory.Sdk.TaskBusiness.BaseBusiness.GetOrderInfo(orderID, userID, agentID);
+            IntFactory.Sdk.OrderBaseEntity orderInfo = result.order;
+            JsonResult.Add("items",orderInfo);
+            return new JsonResult {
+
+                Data = JsonResult,
+                JsonRequestBehavior=JsonRequestBehavior.AllowGet
+            
+            };
+        }
+
 
     }
 }
