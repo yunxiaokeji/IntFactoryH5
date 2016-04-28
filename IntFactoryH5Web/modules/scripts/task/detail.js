00dc2e30fd1e939f89ad08fd696fc9404033f762
song19940906 <2079973693@qq.com>
2016-04-28 10:02:31 +0800

    4.28更新

--- IntFactoryH5Web/modules/scripts/task/detail.js
+++ IntFactoryH5Web/modules/scripts/task/detail.js
@@ -356,7 +356,7 @@
                 $(".shop-status").html("<div class='no-material'>暂无材料</div>");
             }
             else {
-                doT.exec("/template/task/materList.html", function (templateFun) {
+                doT.exec("/modules/template/task/materList.html", function (templateFun) {
                     var innerText = templateFun(data);
                     innerText = $(innerText);
                     $(".shop-status").html(innerText);
@@ -429,7 +429,7 @@
     //获取或添加任务讨论
     TaskDetail.GetOrAddTaskReply = function (data,replyOperate) {
 
-        doT.exec("/template/task/detailReply.html", function (templateFun) {
+        doT.exec("/modules/template/task/detailReply.html", function (templateFun) {
 
             var dataReplys = {};
             dataReplys.items = data.items;
