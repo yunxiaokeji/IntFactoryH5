db291f7514984c98267f85f2d369adf7f673a8ad
xiaoxiaomuge <xiaoxiaomuge@163.com>
2016-05-20 20:26:13 +0800

    调整配置文件

--- IntFactory.Sdk/AppConfig.cs
+++ IntFactory.Sdk/AppConfig.cs
@@ -8,9 +8,9 @@ namespace IntFactory.Sdk
 {
     public class AppConfig
     {
-        public static string ApiUrl =System.Configuration.ConfigurationManager.AppSettings["ApiUrl"]?? "http://90cloudsales.com";
+        public static string ApiUrl = System.Configuration.ConfigurationManager.AppSettings["ApiUrl"] ?? "http://zngc.intfactory.cn";
         public static string AppKey = "BC6802E9-285C-471C-8172-3867C87803E2";
         public static string AppSecret = "9F8AF979-8A3B-4E23-B19C-AB8702988466";
-        public static string CallBackUrl = "http://gyl.movedbuy.com";
+        public static string CallBackUrl = "http://zngc.intfactory.cn";
     }
 }
